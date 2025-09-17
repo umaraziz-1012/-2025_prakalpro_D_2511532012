179be73d1cbcc7910c22273c01b49b8bb35010f2
Umar Abdul Aziz <Umar Abdul Aziz@LAPTOP-17OB2PNH>
2025-09-17 14:37:04 +0700

    https://github.com/UmarAbdulAziz-unand/2025_praklpro_D_2511532012

---
+++ praalpro_2025_D_2511532012/src/Pekan1/Hellowoarld.java
@@ -0,0 +1,15 @@
+package Pekan1;
+
+public class Hellowoarld {
+
+	public static void main(String[] args) {
+		System.out.println ("hello world");
+		System.out.println();
+		System.out.println("program ini meghasilkan");
+		System.out.println("empat baris output");
+		System.out.println("/\\///\\\\///\\/");
+
+
+	}
+
+}
